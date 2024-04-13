6e20d06567d8535cb5363c58e878441bbef16592
JohanChane <johanchanex@gmail.com>
2024-04-13 22:25:54 +0800

    Don't save the clash config each exit. Perfect the execution of system cmd.

--- clashtui/src/utils/tui/impl_clashsrv.rs
+++ clashtui/src/utils/tui/impl_clashsrv.rs
@@ -1,6 +1,10 @@
 use super::ClashTuiUtil;
 use crate::tui::tabs::ClashSrvOp;
-use crate::utils::ipc::{self, exec};
+use crate::utils::{
+    ipc::{self, exec},
+    utils as toolkit,
+};
+
 use std::io::Error;
 
 impl ClashTuiUtil {
@@ -11,30 +15,49 @@ impl ClashTuiUtil {
                 if self.tui_cfg.is_user {
                     args.push("--user")
                 }
-                exec("systemctl", args)?;
-                exec(
+                let output1 = exec("systemctl", args)?;     // Although the command execution is successful,
+                                                            // the operation may not necessarily be successful.
+                                                            // So we need to show the command's output to the user.
+                let output2 = exec(
                     "systemctl",
                     vec!["status", self.tui_cfg.clash_srv_name.as_str()],
-                )
+                )?;
+
+                Ok(format!("# ## restart\n{output1}# ## status\n{output2}"))
             }
             ClashSrvOp::StopClashService => {
                 let mut args = vec!["stop", self.tui_cfg.clash_srv_name.as_str()];
                 if self.tui_cfg.is_user {
                     args.push("--user")
                 }
-                exec("systemctl", args)?;
-                exec(
+                let output1 = exec("systemctl", args)?;
+                let output2 = exec(
                     "systemctl",
                     vec!["status", self.tui_cfg.clash_srv_name.as_str()],
-                )
+                )?;
+
+                Ok(format!("# ## stop\n{output1}# ## status\n{output2}"))
             }
-            ClashSrvOp::SetPermission => ipc::exec_with_sbin(
-                "setcap",
-                vec![
-                    "'cap_net_admin,cap_net_bind_service=+ep'",
-                    self.tui_cfg.clash_core_path.as_str(),
-                ],
-            ),
+            ClashSrvOp::SetPermission => {
+                let pgm = "setcap";
+                let args = vec!["cap_net_admin,cap_net_bind_service=+ep",
+                            self.tui_cfg.clash_core_path.as_str(),
+                            ];
+                if toolkit::is_run_as_root() {
+                    ipc::exec_with_sbin(
+                        pgm,
+                        args,
+                    )
+                } else {
+                    let mut cmd = vec![pgm];
+                    cmd.extend(args);
+                    // `setcap` doesn't trigger the polkit agent.
+                    ipc::exec_with_sbin(
+                        "pkexec",
+                        cmd,
+                    )
+                }
+            }
             _ => Err(Error::new(
                 std::io::ErrorKind::NotFound,
                 "No Support Action",
