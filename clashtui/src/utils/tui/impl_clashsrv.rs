1a8f92a3fae969a726c693b9a425641d3ae7bebe
Jackhr-arch <63526062+Jackhr-arch@users.noreply.github.com>
2024-05-03 13:34:56 +0000

    fix user mode service's status check

--- clashtui/src/utils/tui/impl_clashsrv.rs
+++ clashtui/src/utils/tui/impl_clashsrv.rs
@@ -15,13 +15,14 @@ impl ClashTuiUtil {
                 if self.tui_cfg.clash_srv_is_user {
                     args.push("--user")
                 }
-                let output1 = exec("systemctl", args)?;     // Although the command execution is successful,
-                                                            // the operation may not necessarily be successful.
-                                                            // So we need to show the command's output to the user.
-                let output2 = exec(
-                    "systemctl",
-                    vec!["status", self.tui_cfg.clash_srv_name.as_str()],
-                )?;
+                let output1 = exec("systemctl", args)?; // Although the command execution is successful,
+                                                        // the operation may not necessarily be successful.
+                                                        // So we need to show the command's output to the user.
+                args = vec!["status", self.tui_cfg.clash_srv_name.as_str()];
+                if self.tui_cfg.clash_srv_is_user {
+                    args.push("--user")
+                }
+                let output2 = exec("systemctl", args)?;
 
                 Ok(format!("# ## restart\n{output1}# ## status\n{output2}"))
             }
@@ -31,31 +32,27 @@ impl ClashTuiUtil {
                     args.push("--user")
                 }
                 let output1 = exec("systemctl", args)?;
-                let output2 = exec(
-                    "systemctl",
-                    vec!["status", self.tui_cfg.clash_srv_name.as_str()],
-                )?;
+                args = vec!["status", self.tui_cfg.clash_srv_name.as_str()];
+                if self.tui_cfg.clash_srv_is_user {
+                    args.push("--user")
+                }
+                let output2 = exec("systemctl", args)?;
 
                 Ok(format!("# ## stop\n{output1}# ## status\n{output2}"))
             }
             ClashSrvOp::SetPermission => {
                 let pgm = "setcap";
-                let args = vec!["cap_net_admin,cap_net_bind_service=+ep",
-                            self.tui_cfg.clash_core_path.as_str(),
-                            ];
+                let args = vec![
+                    "cap_net_admin,cap_net_bind_service=+ep",
+                    self.tui_cfg.clash_core_path.as_str(),
+                ];
                 if toolkit::is_run_as_root() {
-                    ipc::exec_with_sbin(
-                        pgm,
-                        args,
-                    )
+                    ipc::exec_with_sbin(pgm, args)
                 } else {
                     let mut cmd = vec![pgm];
                     cmd.extend(args);
                     // `setcap` doesn't trigger the polkit agent.
-                    ipc::exec_with_sbin(
-                        "pkexec",
-                        cmd,
-                    )
+                    ipc::exec_with_sbin("pkexec", cmd)
                 }
             }
             _ => Err(Error::new(
