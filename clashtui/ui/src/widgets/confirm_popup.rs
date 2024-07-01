dde901693439501bf0cd116f99cc34688ad6074f
Jackhr <63526062+Jackhr-arch@users.noreply.github.com>
2024-07-01 00:39:31 +0000

    expend usage of confirm popup

--- clashtui/ui/src/widgets/confirm_popup.rs
+++ clashtui/ui/src/widgets/confirm_popup.rs
@@ -6,14 +6,22 @@ use crate::{EventState, Infailable};
 use super::MsgPopup;
 /// Modified [MsgPopup]
 ///
-/// Add 'y', 'n'/Esc to close
+/// Add 'y'/Enter, 'n'/Esc to close
+///
+/// NOTE: default work in confirm mode, call `ConfirmPopup::should_confirm`
+///  to change the behavior
 ///
 /// Not impl [Visibility][crate::Visibility] since [MsgPopup] does
-pub struct ConfirmPopup(MsgPopup);
+#[derive(Default)]
+pub struct ConfirmPopup(MsgPopup, bool);
 
 impl ConfirmPopup {
     pub fn new() -> Self {
-        Self(MsgPopup::default())
+        Self(MsgPopup::default(), false)
+    }
+
+    pub fn should_confirm(&mut self, should: bool) {
+        self.1 = should
     }
 
     pub fn event(&mut self, ev: &Event) -> Result<EventState, Infailable> {
@@ -27,11 +35,11 @@ impl ConfirmPopup {
                 return Ok(EventState::NotConsumed);
             }
             match key.code {
-                KeyCode::Char('y') => {
+                KeyCode::Char('y') | KeyCode::Enter if self.1 => {
                     self.0.hide();
                     return Ok(EventState::Yes);
                 }
-                KeyCode::Char('n') | KeyCode::Esc => {
+                KeyCode::Char('n') | KeyCode::Esc if self.1 => {
                     self.0.hide();
                     return Ok(EventState::Cancel);
                 }
