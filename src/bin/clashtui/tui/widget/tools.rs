f09a4cfa0c4626997e2dc619f79607cf2f2efe6a
Jackhr-arch <63526062+Jackhr-arch@users.noreply.github.com>
2024-09-05 15:59:53 +0000

    mark item under tools as deprecated

--- src/bin/clashtui/tui/widget/tools.rs
+++ src/bin/clashtui/tui/widget/tools.rs
@@ -1,50 +1,34 @@
 use ratatui::prelude as Ra;
 use Ra::{Constraint, Direction, Layout, Rect};
-
+#[deprecated = "use `centered_rect` instand"]
 /// Create a centered rect using up certain percentage of the available rect `r`
 pub fn centered_percent_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
-    let popup_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(
-            [
-                Constraint::Percentage((100 - percent_y) / 2),
-                Constraint::Percentage(percent_y),
-                Constraint::Percentage((100 - percent_y) / 2),
-            ]
-            .as_ref(),
-        )
-        .split(r);
-
-    Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage((100 - percent_x) / 2),
-                Constraint::Percentage(percent_x),
-                Constraint::Percentage((100 - percent_x) / 2),
-            ]
-            .as_ref(),
-        )
-        .split(popup_layout[1])[1]
+    centered_rect(
+        Constraint::Percentage(percent_x),
+        Constraint::Percentage(percent_y),
+        r,
+    )
 }
 
-/// Create a centered rect using specific lengths for width and height
-pub fn centered_lenght_rect(width: u16, height: u16, container: Rect) -> Rect {
-    let popup_layout = Layout::default()
+/// Create a centered [Rect] from `raw`
+pub fn centered_rect(width: Constraint, height: Constraint, raw: Rect) -> Rect {
+    let he = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Length((container.height - height) / 2),
-            Constraint::Length(height),
-            Constraint::Length((container.height - height) / 2),
-        ])
-        .split(container);
+        .constraints([Constraint::Min(0), height, Constraint::Min(0)])
+        .split(raw);
 
     Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Length((container.width - width) / 2),
-            Constraint::Length(width),
-            Constraint::Length((container.width - width) / 2),
-        ])
-        .split(popup_layout[1])[1]
+        .constraints([Constraint::Min(0), width, Constraint::Min(0)].as_ref())
+        .split(he[1])[1]
+}
+
+#[deprecated = "use `centered_rect` instand"]
+/// Create a centered rect using specific lengths for width and height
+pub fn centered_lenght_rect(width: u16, height: u16, container: Rect) -> Rect {
+    centered_rect(
+        Constraint::Length(width),
+        Constraint::Length(height),
+        container,
+    )
 }
